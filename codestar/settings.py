fc75372361a51608bf537a85b507b7a63954b920
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-08-29 12:32:09 +0100

    Heroku publish setting

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+plyk*b^0^$i9fr_8=95t+1zjartvxhxc904bfm7_fk)sn)p@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', '.herokuapp.com', ]
 
 
 # Application definition
